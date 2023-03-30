78a0f5781b6b99ff83226cc69fc8032c7b09df6b
c.huang <chuang@netprotections.co.jp>
2023-03-30 15:16:37 +0800

    make rails c runnable

---
+++ app/apis/v1/root.rb
@@ -0,0 +1,12 @@
+module V1
+
+  class Root < Grape::API
+
+    version 'v1'
+    format :json
+    content_type :json, 'application/json'
+
+
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module GoodBody
   class Application < Rails::Application
+
+    config.load_defaults 5.2
+
+    config.time_zone = 'Taipei'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +26,5 @@ module GoodBody
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
