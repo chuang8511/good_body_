6b9952afd185081fac76a0cfb94a7b26039f9e71
Horatio <eddiefuh123@gmail.com>
2023-04-12 16:28:51 +0800

    horatio_0412_2

--- app/apis/v1/register_account_api.rb
+++ app/apis/v1/register_account_api.rb
@@ -16,37 +16,49 @@ module V1
             end
 
             post do
-                
-                #if params[account].nil? || params[password].nil? || params[name].nil? || params[phone_number].nil? || params[mail].nil?
+                j=0
+                if params[:account].nil? || params[:password].nil? || params[:name].nil? || params[:phone_number].nil? || params[:mail].nil?
+                    
                     #必填欄位空白
+                    {context: '必填欄位空白'}
+                    j=1
+                    #test: "1"
                     #context: 'at least one of the required fields is empty '
-                #end
+                end
 
                 
-"""
-                user=User.find_by(account: params[mail])
+
+                user=User.find_by(mail: params[mail])
                 if user!=nil
                     #此mail已存在
-                    context: 'email duplicated'
+                    j=1
+                    {context: '郵件已存在'}
+                    #test: "2"
+                    #context: 'email duplicated'
                 else
                     user=User.find_by(account: params[account])
                     if user!=nil
                         #此帳號已存在
-                        context: 'account already exist'
+                        j=1
+                        {context: '帳號已存在'}
+                        #test: "3"
+                        #context: 'account already exist'
 
                     end
                 end
-"""
-                User.create!(account:params[:account],password:params[:password],name:params[:name],phone_number:params[:phone_number],height:params[:height],weight:params[:weight],age:params[:age])
+            
+                #以上有問題
+
+
+                if j=0
+                User.create!(account:params[:account],password:params[:password],name:params[:name],phone_number:params[:phone_number],height:params[:height],weight:params[:weight],age:params[:age],mail:params[:mail])
+                
                 #params[:account],params[:password],params[:name],params[:phone_number],params[:height],params[:weight],params[:age]
-
+                {context: 'success'}
+                end
             end
-
         end
-    
-
-
+        
     end
-
-
+    
 end
