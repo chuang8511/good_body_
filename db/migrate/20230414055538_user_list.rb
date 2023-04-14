52c999fc1f657e7f8877aa865a10a7454fd707bf
Isa Chi <chirumvu@gmail.com>
2023-04-14 14:06:03 +0800

    Isa_20230414_1405

---
+++ db/migrate/20230414055538_user_list.rb
@@ -0,0 +1,17 @@
+class UserList < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :account
+      t.string :password
+      t.string :name
+      t.text :phone_number
+      t.string :height
+      t.string :weight
+      t.integer :age
+      t.text :mail
+
+
+      t.timestamps
+    end
+  end
+end
