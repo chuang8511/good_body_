c4b789a69de100f01412184b728296192fcc6c8c
Isa Chi <chirumvu@gmail.com>
2023-05-12 17:29:34 +0800

    Isa_20230512_1729

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,3 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+config.consider_all_requests_local = true
+config.action_view.raise_on_missing_translations = true
+config.action_controller.allow_forgery_protection = true
