30a341e6cf54901d7bbcdfa7f355b0fc000bb6ba
Isa Chi <chirumvu@gmail.com>
2023-05-15 18:19:58 +0800

    Isa_20230515_1819
    Views and Controllers of login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,20 +1,20 @@
-require 'uri'
-require 'net/http'
-
 class LoginController < ApplicationController
 
   def index
   end
 
   def login
-    uri = URI("http://localhost:3000/v1/login")
-    response = Net::HTTP.post_form(uri, email: params[:email], password: params[:password])
 
-    if response.code == "200"
+    response = ApiCaller.call_api('post', '1','login', { email: params[:email], password: params[:password] })
+
+    if response[:code] == 200
       redirect_to root_url, notice: "Logged in!"
+      
     else
-      flash.now.alert = "Email or password is invalid."
+      flash.now[:error] = "Email or password is invalid. #{response}"
       render :index
     end
+
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'v1/login', to: 'login#index'
-  post 'v1/login', to: 'login#login'
+  get '/v1/login', to: 'login#index'
+  post '/v1/login', to: 'login#login'
   mount EndpointResource => '/'
 end
