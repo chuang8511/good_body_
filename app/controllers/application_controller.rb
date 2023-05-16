ddffc5534fb09e9ca710e27c5c929574ba3ebb5a
Horatio <eddiefuh123@gmail.com>
2023-05-16 16:15:03 +0800

    0516_02_horatio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token
+    def current_user
+        #return unless session[:user_id]
+        @current_user ||= session[:user_id]&&User.find(session[:user_id])
+    end
+
     def index
     end
 end
--- app/controllers/register_account_controller.rb
+++ app/controllers/register_account_controller.rb
@@ -1,64 +1,29 @@
 require 'net/http'
 require 'json'
 class RegisterAccountController < ApplicationController
+
     def index
         
     end
 
-    def new
-    end
 
     def submit
+        @register_account_user=RegisterAccountUser.new(params[:account],params[:password],params[:name],params[:email],params[:phone_number],params[:age],params[:gender],params[:height],params[:weight])
+        
 
-
-
-
-      response = ApiCaller.call_api('post', '1', 'register_account', {account:params[:account],password:params[:password],name:params[:name],email:params[:email],phone_number:params[:phone_number],age:params[:age],gender:params[:gender],height:params[:height],weight:params[:weight]})
-      status_code = response[0]
-
-      if status_code == 200
-          flash[:success] = 'Success'
-          @result = response[1]
-          render :create_success
-      else
-          flash.now[:error] = "Failed to create account, error code: #{status_code} #{response[1]}"
-          @result = response[1]
+        if @register_account_user.create_account
+        # 成功
+          session[:account]=params[:account]
           render :show
+        else
+        # 失敗
+          render :index
       end
-
-      flash[:error] = nil
     end
 =begin
-      #create_account(params[:account],params[:password],params[:email],params[:phone_number],params[:name],params[:gender],params[:age],params[:height],params[:weight])
-        form_data = params
-
-        uri = URI.parse('http://localhost:3000/v1/register_account')
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.read_timeout = 10
-        request = Net::HTTP::Post.new(uri.path)
-        request['Content-Type'] = 'application/json'
-        request.body = form_data.to_json
-      
-        response = http.request(request)
-   
-        # web cant get response from api. always timeout. token lost?
-        #if response.code.to_i == 200
-          @result = JSON.parse(response.body)
-        render json: { result: 'a' }#:show
-       # else
-          #render json: { result: 'error' }, status: :unprocessable_entity
-       # end
-
-
-        # ...
-    end
-
     private
-
-    def create_account(account,password,email,phone_number,name,gender,age,height,weight)
-      request_api(
-        "http://localhost:3000/v1/register_account/#{URI.encode(name)}"
-      )
+    def user_params
+      params.require(:user).permit(:account,:email,:password,:name,:phone_number,:gender, :age, :height, :weight)
     end
 =end
 end
