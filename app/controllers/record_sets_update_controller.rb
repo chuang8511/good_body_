cc48f52496ce9c89fe82f81b3ebc9518ac7bfd00
闕 暉恩 <w.chueh@mac-tw0027.np.co.jp>
2023-05-22 18:06:41 +0800

    Wayne_20230522_1806
    set_update_view_unfinished

--- app/controllers/record_sets_update_controller.rb
+++ app/controllers/record_sets_update_controller.rb
@@ -2,44 +2,60 @@ require 'uri'
 require 'net/http'
 require_relative '../domain_models/common_error'
 
-# 點選 Update 跳到可輸入環節？ # 需要重新討論流程
-
 class RecordSetsUpdateController < ApplicationController
 
-    id      = params[:id]
-    content = params[:content]
-    set     = params[:set]
-    rep     = params[:rep]
-    weight  = params[:weight]
+    def edit_record
+        id = params[:id]
+        @record = SetsRecord.find_by(id: id)
+    
+        redirect_to error_path unless @record
+    
+        render :edit_record
+    end
+    
+    def update_record
 
-    record_sets_update = RecordSetsUpdate.new(id, content, set, rep, weight)
+        id      = params[:id]
+        @record = SetsRecord.find_by(id: id)
+        content = params[:contents]
+        set     = params[:sets]
+        rep     = params[:reps]
+        weight  = params[:weight]
+
+        record_sets = RecordSetsUpdate.new(id, content, set, rep, weight)
 
 
-    begin
+        begin
 
-        if record_sets_update.update_set_record
-            render :index
+            if record_sets.update_set_record
+                flash[:success] = 'Record updated successfully.'
+                render :index
+            else
+                flash.now[:error] = 'Failed to update record.'
+                render :edit_record
+            end
+
+        rescue NoContentError
+            flash.now[:error] = "#{id}, #{content}, #{set}, #{rep}, #{weight} Record Sets: #{record_sets.inspect}<br>@Record: #{@record.inspect}<br>Content is wrong. Please re-enter your content."
+            render :edit_record
+
+        rescue NoSetError
+            flash.now[:error] = 'Set is wrong. Please re-enter your set.'
+            render :edit_record
+
+        rescue NoRepError
+            flash.now[:error] = 'Rep is wrong. Please re-enter your Rep.'
+            render :edit_record
+
+        rescue NoWeightError
+            flash.now[:error] = 'Weight is wrong. Please re-enter your weight.'
+            render :edit_record
+
+        rescue SameParamsError
+            flash.now[:error] = 'Your input is same as former one.'
+            render :edit_record
+
         end
-
-    rescue NoContentError
-        flash.now[:error] = 'Content is wrong. Please re-enter your content.'
-        render :index
-
-    rescue NoSetError
-        flash.now[:error] = 'Set is wrong. Please re-enter your set.'
-        render :index
-
-    rescue NoRepError
-        flash.now[:error] = 'Rep is wrong. Please re-enter your Rep.'
-        render :index
-
-    rescue NoWeightError
-        flash.now[:error] = 'Weight is wrong. Please re-enter your weight.'
-        render :index
-
-    rescue NoWeightError
-        flash.now[:error] = 'Your input is same as former one.'
-        render :index
 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,20 @@ Rails.application.routes.draw do
   get 'v1/login', to: 'login#new'
   post 'v1/login', to: 'login#login'
   
+  # View Page
   get 'sets_page', to: 'record_sets#index'
 
+  # Delete
   delete 'delete_record/:id', to: 'record_sets_delete#delete_record', as: 'delete_record'
 
-  patch 'update_record/:id', to: 'record_sets_update#update_set_record', as: 'update_record'
+  # Update 
+  get 'edit_record/:id', to: 'record_sets_update#edit_record', as: 'edit_record'
+  patch 'update_record/:id', to: 'record_sets_update#update_record', as: 'update_record'
+  # patch 'record_sets/:id/update_record', to: 'record_sets_update#update_record', as: 'update_record'
+
+
 
 
   mount EndpointResource => '/'
 end
+
