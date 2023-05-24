1a5101d5a9a4933e9d01c8c28b837b42444c9394
Isa Chi <chirumvu@gmail.com>
2023-05-24 10:36:55 +0800

    Isa_20230524_1036

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/v1/login', to: 'login#index'
-  post '/v1/login', to: 'login#login'
+  get '/login', to: 'login#index'
+  post '/login', to: 'login#login'
 
   get '/homepage', to: 'homepage#index', as: 'homepage'
 
-  get 'v1/times_record_create', to: 'times_record_create#new', as: 'create_times_record'
-  post 'v1/times_record_create', to: 'times_record_create#create'
+  get '/times_record_create', to: 'times_record_create#new', as: 'create_times_record'
+  post '/times_record_create', to: 'times_record_create#create'
 
 
 
-  get '/v1/times_record', to: 'times_record_view#index', as: 'times_record'
-  delete '/v1/times_record/:id', to: 'times_record_delete#delete', as: 'delete_times_record'
-  get '/v1/times_record/:id/edit', to: 'times_record_update#edit', as: 'edit_times_record'
-  patch '/v1/times_record/:id', to: 'times_record_update#update', as: 'update_times_record'
+  get '/times_record', to: 'times_record_view#index', as: 'times_record'
+  delete '/times_record/:id', to: 'times_record_delete#delete', as: 'delete_times_record'
+  get '/times_record/:id/edit', to: 'times_record_update#edit', as: 'edit_times_record'
+  patch '/times_record/:id', to: 'times_record_update#update', as: 'update_times_record'
   mount EndpointResource => '/'
 end
